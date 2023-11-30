8b28738db2bcc8f4738abd36f7dfcc1a070914aa
DHAN <75938963+DHAN2303@users.noreply.github.com>
2023-11-30 10:14:21 +0300

    done

--- DragDrop/dist/test-drag-drop/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-Z5KEIKVD.mjs";import"./chunk-OQCSSYE4.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- tf-callange2/README.md
+++
@@ -1,8 +0,0 @@
-# DragChallenge
-
-Demo [Demo](http://46.101.119.205/home)
-
-## Development server
-
-Run `npm install`  and then `ng serve` => Enjoy
-
